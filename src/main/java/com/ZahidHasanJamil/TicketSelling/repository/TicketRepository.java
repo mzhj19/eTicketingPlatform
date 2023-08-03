8268e28df5e15bd8aba73ef182195084f136e884
Md. Zahid Hasan Jamil <mzhj19@gmail.com>
2023-08-03 15:10:53 +0600

    Ticket: ticket search module has been updated

--- src/main/java/com/ZahidHasanJamil/TicketSelling/repository/TicketRepository.java
+++ src/main/java/com/ZahidHasanJamil/TicketSelling/repository/TicketRepository.java
@@ -13,9 +13,10 @@ public interface TicketRepository extends JpaRepository<Ticket, Long> {
     @Query("SELECT t FROM Ticket t " +
             "WHERE (:fromWhere IS NULL OR t.fromWhere = :fromWhere) " +
             "AND (:toWhere IS NULL OR t.toWhere = :toWhere) " +
-            "AND (:ticketType IS NULL OR t.toWhere = :ticketType) " +
+            "AND (:ticketType IS NULL OR t.ticketType = :ticketType) " +
             "AND (:tDate IS NULL OR t.date = :tDate) " +
-            "AND (:price IS NULL OR t.price = :price)")
+            "AND (:price IS NULL OR t.price = :price)"
+    )
     List<Ticket> findBySearchReqDtoObject(
             @Param("fromWhere") String fromWhere,
             @Param("toWhere") String toWhere,
