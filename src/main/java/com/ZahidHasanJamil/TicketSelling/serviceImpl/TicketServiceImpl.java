4335d53954496b505676970318eb2575b81c71a5
Md. Zahid Hasan Jamil <mzhj19@gmail.com>
2023-08-07 20:59:27 +0600

    Edit: Ticket edit module has been updated

--- src/main/java/com/ZahidHasanJamil/TicketSelling/serviceImpl/TicketServiceImpl.java
+++ src/main/java/com/ZahidHasanJamil/TicketSelling/serviceImpl/TicketServiceImpl.java
@@ -57,9 +57,10 @@ public class TicketServiceImpl implements TicketService {
     }
 
     @Override
-    public Optional<Ticket> editTicket(Long id, Ticket updateData) {
+    public Optional<Ticket> editTicket(String token, Long id, Ticket updateData) {
         Ticket previousTicket = ticketRepository.findById(id).orElseThrow(null);
-        if (previousTicket == null) return null;
+        User user = userRepository.findByEmail(jwtService.extractUsername(token.substring(7))).orElse(null);
+        if (user == null || !previousTicket.getSellerEmail().equals(user.getEmail()) || previousTicket == null) return null;
 
         previousTicket.setTicketType(updateData.getTicketType());
         previousTicket.setSeller(previousTicket.getSeller());
